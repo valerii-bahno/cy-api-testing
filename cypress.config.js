abc1ba13b3dd694d83b34159061fb70115c83d0d
valeriibahno <valeriybagno89@gmail.com>
2025-06-04 14:18:44 +0300

    Fix

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default defineConfig({
-  // projectId: process.env.PROJECT_ID,
-  projectId: Cypress.env('PROJECT_ID'),
+  projectId: process.env.PROJECT_ID,
   viewportHeight: 1080,
   viewportWidth: 1920,
   e2e: {
