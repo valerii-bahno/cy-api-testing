853d356726fa8da2c9d01899cf62d310eff407c6
valeriibahno <valeriybagno89@gmail.com>
2025-06-05 10:47:00 +0300

    Added second test

---
+++ cypress/e2e/secondTest.spec.js
@@ -0,0 +1,13 @@
+/ <reference types="cypress" />
+
+describe('Test log out', () => {
+    beforeEach('login to application', () => {
+        cy.loginToApplication()
+    })
+
+    it('Verify user can log out', () => {
+        cy.contains('Settings').click()
+        cy.contains('Or click here to logout').click()
+        cy.get('.navbar-nav').should('contain', 'Sign up')
+    })
+})
