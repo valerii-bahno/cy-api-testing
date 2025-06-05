72c959cfb48716a5d6c665233046c75888bd4c04
valeriibahno <valeriybagno89@gmail.com>
2025-06-05 20:34:12 +0300

    Fixed post-cond

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,105 +1,108 @@
 describe('Test with backend', () => {
+    beforeEach('login to application', () => {
+        cy.intercept({ method: 'Get', path: 'tags' }, { fixture: 'tags.json' }).as('getTags');
+        cy.loginToApplication();
+        cy.wait('@getTags');
+    });
 
-  beforeEach('login to application', () => {
-    cy.intercept({ method: 'Get', path: 'tags' }, { fixture: 'tags.json' }).as('getTags')
-    cy.loginToApplication()
-    cy.wait('@getTags')
-  })
+    after('Cleanup article via API', () => {
+        cy.get('@token').then((token) => {
+            cy.request({
+                method: 'GET',
+                url: 'https://conduit-api.bondaracademy.com/api/articles?limit=10&offset=0',
+                headers: { Authorization: `Token ${token}` }
+            }).then((res) => {
+                const target = res.body.articles.find((a) => a.title === 'This is the title');
+                if (target) {
+                    cy.request({
+                        method: 'DELETE',
+                        url: `https://conduit-api.bondaracademy.com/api/articles/${target.slug}`,
+                        headers: { Authorization: `Token ${token}` }
+                    });
+                }
+            });
+        });
+    });
 
-  after('Cleanup article via API', () => {
-  cy.get('@token').then((token) => {
-    cy.request({
-      method: 'GET',
-      url: 'https://conduit-api.bondaracademy.com/api/articles?limit=10&offset=0',
-      headers: { Authorization: `Token ${token}` },
-    }).then((res) => {
-      const target = res.body.articles.find((a) => a.title === 'This is the title')
-      if (target) {
-        cy.request({
-          method: 'DELETE',
-          url: `https://conduit-api.bondaracademy.com/api/articles/${target.slug}`,
-          headers: { Authorization: `Token ${token}` },
-        })
-      }
-    })
-  })
-})
+    it('Verify correct request and response', () => {
+        cy.intercept('POST', 'https://conduit-api.bondaracademy.com/api/articles/').as('postArticles');
 
-  it('Verify correct request and response', () => {
-    cy.intercept('POST', 'https://conduit-api.bondaracademy.com/api/articles/').as('postArticles')
+        cy.contains('New Article').click();
+        cy.get('[formcontrolname="title"]').type('This is the title');
+        cy.get('[formcontrolname="description"]').type('This is the description without intercepting');
+        cy.get('[formcontrolname="body"]').type('This is the body');
+        cy.contains('Publish Article').click();
 
-    cy.contains('New Article').click()
-    cy.get('[formcontrolname="title"]').type('This is the title')
-    cy.get('[formcontrolname="description"]').type('This is the description without intercepting')
-    cy.get('[formcontrolname="body"]').type('This is the body')
-    cy.contains('Publish Article').click()
+        cy.wait('@postArticles').then((xhr) => {
+            expect(xhr.response.statusCode).to.equal(201);
+            expect(xhr.request.body.article.body).to.equal('This is the body');
+            expect(xhr.response.body.article.description).to.equal('This is the description without intercepting');
+        });
+    });
 
-    cy.wait('@postArticles').then( xhr => {
-      expect(xhr.response.statusCode).to.equal(201)
-      expect(xhr.request.body.article.body).to.equal('This is the body')
-      expect(xhr.response.body.article.description).to.equal('This is the description without intercepting')
-    })
-  })
+    it('Verify popular tags are displayed', () => {
+        cy.get('.tag-list').should('contain', 'Cypress').and('contain', 'Api').and('contain', 'GitHub');
+    });
 
-  it('Verify popular tags are displayed', () => {
-    cy.get('.tag-list')
-    .should('contain', 'Cypress')
-    .and('contain', 'Api')
-    .and('contain', 'GitHub')
-  })
+    it('Verify global feed likes count', { retries: 2 }, () => {
+        cy.intercept('GET', '**/api/articles*', { fixture: 'articles.json' }).as('updateArticles');
 
-  it('Verify global feed likes count', { retries: 2 }, () => {
-    cy.intercept('GET', '**/api/articles*', { fixture: 'articles.json'}).as('updateArticles')
+        cy.contains('Global Feed').click();
+        cy.wait('@updateArticles', { timeout: 10000 });
+        cy.get('app-favorite-button button')
+            .should('have.length', 2)
+            .then((heartList) => {
+                expect(heartList[0]).to.contain('10');
+                expect(heartList[1]).to.contain('20');
+            });
 
-    cy.contains('Global Feed').click()
-    cy.wait('@updateArticles', { timeout: 10000 })
-    cy.get('app-favorite-button button').should('have.length', 2).then( heartList => {
-      expect(heartList[0]).to.contain('10')
-      expect(heartList[1]).to.contain('20')
-    })
+        cy.fixture('articles').then((file) => {
+            const articleLink = file.articles[1].slug;
+            file.articles[1].favoritesCount = 21;
 
-    cy.fixture('articles').then( file => {
-      const articleLink = file.articles[1].slug
-      file.articles[1].favoritesCount = 21
+            cy.intercept('POST', `https://conduit-api.bondaracademy.com/api/articles/${articleLink}/favorite`, {
+                statusCode: 200,
+                body: file
+            });
+        });
 
-      cy.intercept('POST', `https://conduit-api.bondaracademy.com/api/articles/${articleLink}/favorite`, { statusCode: 200, body: file })
-    })
+        cy.get('app-article-list button').eq(1).click().should('contain', '21');
+    });
 
-    cy.get('app-article-list button').eq(1).click().should('contain', '21')
-  })
+    it('Delete a new article in a global feed', () => {
+        const bodyRequest = {
+            article: {
+                tagList: [],
+                title: 'Request from API',
+                description: 'Description for API',
+                body: 'Body for request'
+            }
+        };
 
-  it('Delete a new article in a global feed', () => {
-    const bodyRequest = {
-      "article": {
-        "tagList": [],
-        "title": "Request from API",
-        "description": "Description for API",
-        "body": "Body for request",
-      }
-    }
+        cy.get('@token').then((token) => {
+            cy.request({
+                url: 'https://conduit-api.bondaracademy.com/api/articles/',
+                headers: { Authorization: 'Token ' + token },
+                method: 'POST',
+                body: bodyRequest
+            }).then((response) => {
+                expect(response.status).to.equal(201);
+            });
 
-    cy.get('@token').then( token => {
-      cy.request({
-        url: 'https://conduit-api.bondaracademy.com/api/articles/',
-        headers: { 'Authorization': 'Token ' + token },
-        method: 'POST',
-        body: bodyRequest
-      }).then( response => {
-        expect(response.status).to.equal(201)
-      })
+            cy.contains('Global Feed').click();
+            cy.get('.article-preview').should('contain', 'Request from API');
+            cy.get('.article-preview').first().click();
+            cy.get('.article-actions').contains('Delete Article').click();
 
-      cy.contains('Global Feed').click()
-      cy.get('.article-preview').should('contain', 'Request from API')
-      cy.get('.article-preview').first().click()
-      cy.get('.article-actions').contains('Delete Article').click()
-
-      cy.request({
-        url: 'https://conduit-api.bondaracademy.com/api/articles?limit=10&offset=0',
-        headers: { 'Authorization': 'Token ' + token },
-        method: 'GET'
-      }).its('body').then( body => {
-        expect(body.articles[0].title).not.to.equal('Request from API')
-      })
-    })
-  })
-})
+            cy.request({
+                url: 'https://conduit-api.bondaracademy.com/api/articles?limit=10&offset=0',
+                headers: { Authorization: 'Token ' + token },
+                method: 'GET'
+            })
+                .its('body')
+                .then((body) => {
+                    expect(body.articles[0].title).not.to.equal('Request from API');
+                });
+        });
+    });
+});
