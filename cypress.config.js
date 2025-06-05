0779d60802aaa67549f773fb32a69aef341d5c72
valeriibahno <valeriybagno89@gmail.com>
2025-06-05 20:41:54 +0300

    Fixed

--- cypress.config.js
+++ cypress.config.js
@@ -20,9 +20,6 @@ export default defineConfig({
 
   e2e: {
     defaultCommandTimeout: 10000,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     baseUrl: 'https://conduit.bondaracademy.com/',
     specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}',
   },
